14adcfb61cc81dfc335f52d5e00c81b1e158eae4
brayanmr24 <brayanmr24@ciencias.unam.mx>
2025-06-23 13:24:06 -0600

    Modificaciones para la agilización de la toma de datos

--- Toma y tratamiento de datos.jl
+++ Toma y tratamiento de datos.jl
@@ -1,18 +1,18 @@
 function toma_de_medidas(duracion)
+    historial = []
     for i ∈ 1:duracion
         datos = String(read(info_serial))
         push!(historial,datos)
         sleep(5)
     end
+    for i ∈ historial
+        x=x*i
+    end
 end
 
-function tratamiento_de_datos(historial)
-    x = ""
+function tratamiento_de_datos(x)
     datos_2 = []
     matrices = []
-    for i ∈ historial
-        x = x*i
-    end
     datos = split(x,"\r\n",keepempty=false)
     for i ∈ 2:length(datos)-1
         lista_1 = split(datos[i],", ",keepempty=false)
@@ -20,7 +20,7 @@ function tratamiento_de_datos(historial)
         push!(datos_2,lista_1)
     end
     for i ∈ datos_2
-        matriz_i = reverse(reshape(i,10,10)',dims=1)
+        matriz_i = reverse(reshape(i,10,20)',dims=1)
         push!(matrices,matriz_i)
     end
     return matrices
